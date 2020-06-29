27604b76ccfeb8b48c816b54cee3327a64f79235
Afront <3943720+Afront@users.noreply.github.com>
2020-06-29 19:33:23 +0900

    Lint benchmark script

--- tests/benchmark.rb
+++ tests/benchmark.rb
@@ -5,15 +5,15 @@ require 'fileutils'
 require 'open3'
 
 module RBSBenchmark
-  @@benchmark_dir = "#{__dir__}/benchmark_inputs"
-  @@benchmark_tmp_dir = "#{__dir__}/tmp/benchmark"
-  @@token_classes = %w[Token TurnToken CodeTkn Char]
-  @@interpreter_classes = %w[
+  @benchmark_dir = "#{__dir__}/benchmark_inputs"
+  @benchmark_tmp_dir = "#{__dir__}/tmp/benchmark"
+  @token_classes = %w[Token TurnToken CodeTkn Char]
+  @interpreter_classes = %w[
     Lexer Parser
     CodeGenerator CodeOptimizer
     TargetCodeGenerator Interpreter
   ]
-  @@copy_to_input_file = ->(input_name) { FileUtils.cp("#{@@benchmark_dir}/#{input_name}", '.input') }
+  @copy_to_input_file = ->(input_name) { FileUtils.cp("#{@benchmark_dir}/#{input_name}", '.input') }
 
   class BenchmarkReportHelper
     def initialize(file, benchmark, copy_to_input_file_lambda)
@@ -47,30 +47,30 @@ module RBSBenchmark
   end
 
   def setup_benchmark
-    setup_path @@benchmark_dir
-    setup_path @@benchmark_tmp_dir
-    $stdout = File.new("#{@@benchmark_tmp_dir}/benchmark_#{Time.new.strftime('%Y%m%d_%H%M%S')}.txt", 'w')
+    setup_path @benchmark_dir
+    setup_path @benchmark_tmp_dir
+    $stdout = File.new("#{@benchmark_tmp_dir}/benchmark_#{Time.new.strftime('%Y%m%d_%H%M%S')}.txt", 'w')
     $stdout.sync = true
   end
 
   def run_benchmark
     setup_benchmark
 
-    Dir.entries(@@benchmark_dir).reject { |f| File.directory? f }.each do |file|
+    Dir.entries(@benchmark_dir).reject { |f| File.directory? f }.each do |file|
       puts file
       puts '-------------------------------------'
       Benchmark.bm do |x|
-        benchmark_reporter = BenchmarkReportHelper.new(file, x, @@copy_to_input_file)
+        benchmark_reporter = BenchmarkReportHelper.new(file, x, @copy_to_input_file)
         benchmark_reporter.report('Vanilla') { `ruby ../compiler.rb` }
-        benchmark_reporter.report('No test') { execute_compiler(targets: ['Steep']) }
-        benchmark_reporter.report('Lexer') { execute_compiler(targets: ['Lexer']) }
+        benchmark_reporter.report('No test') { execute_compiler(targets: %w[Steep]) }
+        benchmark_reporter.report('Lexer') { execute_compiler(targets: %w[Lexer]) }
         benchmark_reporter.report('Parser') { execute_compiler(targets: %w[Parser]) }
         benchmark_reporter.report('Parser+Lexer') { execute_compiler(targets: %w[Lexer Parser]) }
         benchmark_reporter.report('Whole Interpreter') do
-          execute_compiler(targets: @@interpreter_classes)
+          execute_compiler(targets: @interpreter_classes)
         end
         benchmark_reporter.report('All classes') do
-          execute_compiler(targets: @@interpreter_classes + @@token_classes)
+          execute_compiler(targets: @interpreter_classes + @token_classes)
         end
       end
       puts "\n"
