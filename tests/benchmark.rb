f1e39ff4aec7ef9dc3cfbdc8fe90f79547a87e31
Afront <3943720+Afront@users.noreply.github.com>
2020-06-29 18:29:33 +0900

    Update benchmark script

--- tests/benchmark.rb
+++ tests/benchmark.rb
@@ -2,25 +2,24 @@
 
 require 'benchmark'
 require 'fileutils'
-require "open3"
 # require_relative '../compiler'
 
 def copy_to_input_file(input_name)
   FileUtils.cp("./benchmark_inputs/#{input_name}", '.input')
 end
 
-$stdout = File.new("benchmark_#{Time.new.strftime("%Y%m%d_%H%M%S")}.txt", 'w')
+$stdout = File.new("benchmark_#{Time.new.strftime('%Y%m%d_%H%M%S')}.txt", 'w')
 $stdout.sync = true
 
 def execute_compiler(targets:)
   env = {
-    "RBS_TEST_TARGET" => targets.join(","),
-    "RBS_TEST_OPT" => "-I../sig"
+    'RBS_TEST_TARGET' => targets.join(','),
+    'RBS_TEST_OPT' => '-I../sig'
   }
 
-  out, status = Open3.capture2e(env, "ruby", "-rrbs/test/setup", "../compiler.rb")
+  out, status = Open3.capture2e(env, 'ruby', '-rrbs/test/setup', '../compiler.rb')
 
-  STDERR.puts out
+  warn out
 
   raise unless status.success?
 end
@@ -33,26 +32,26 @@ Dir.entries('./benchmark_inputs').reject { |f| File.directory? f }.each do |file
     x.report('vanilla:') { `ruby ../compiler.rb` }
     p
     copy_to_input_file file
-    x.report('No test:') { execute_compiler(targets: ["Steep"]) }
+    x.report('No test:') { execute_compiler(targets: ['Steep']) }
     p
     copy_to_input_file file
-    x.report('Lexer:') { execute_compiler(targets: ["Lexer"]) }
+    x.report('Lexer:') { execute_compiler(targets: ['Lexer']) }
     p
     copy_to_input_file file
-    x.report('Parser:') { execute_compiler(targets: %w(Parser)) }
+    x.report('Parser:') { execute_compiler(targets: %w[Parser]) }
     p
     copy_to_input_file file
-    x.report('Parser+Lexer:') { execute_compiler(targets: %w(Lexer Parser)) }
+    x.report('Parser+Lexer:') { execute_compiler(targets: %w[Lexer Parser]) }
     p
     copy_to_input_file file
-    x.report('Whole Interpreter:') { 
-      execute_compiler(targets: %w(Lexer Parser CodeGenerator CodeOptimizer TargetCodeGenerator Interpreter))
-    }
+    x.report('Whole Interpreter:') do
+      execute_compiler(targets: %w[Lexer Parser CodeGenerator CodeOptimizer TargetCodeGenerator Interpreter])
+    end
     p
     copy_to_input_file file
-    x.report('All classes:') {
-      execute_compiler(targets: %w(Token TurnToken CodeTkn Char Lexer Parser CodeGenerator CodeOptimizer TargetCodeGenerator Interpreter))
-    }
+    x.report('All classes:') do
+      execute_compiler(targets: %w[Token TurnToken CodeTkn Char Lexer Parser CodeGenerator CodeOptimizer TargetCodeGenerator Interpreter])
+    end
   end
   puts "\n"
 
